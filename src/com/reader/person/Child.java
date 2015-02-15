2517ca8b81c02b00e2f0cea0c729adcda9f3a9f9
Kurt Kampa <kurt.kampa@bbd.ca>
2015-02-15 11:58:43 -0500

    updated child object

--- src/com/reader/person/Child.java
+++ src/com/reader/person/Child.java
@@ -1,70 +1,94 @@
 package com.reader.person;
 
 public class Child {
-	private String employeenum;
-	private String childfirstname;
-	private String childlastname;
-	private String childtype;
+    private String certNum;
+    private String childLastName;
+    private String childFirstName;
+    private String birthDate;
+    private String sex;
+    private String relation;
+    private String school;
+
 	
 		        
-	  public Child(String employeenum, String childfirstname, String childlastname, String childtype) {
-	      this.employeenum = employeenum;
-		  this.childfirstname = childfirstname;
-		  this.childlastname = childlastname;
-		  this.childtype = childtype;
-	  }
+    public Child(String certNum, String childFirstName, String childLastName, 
+                  String birthDate, String sex, String relation, String school) {
+              this.certNum = certNum;
+              this.childFirstName = childFirstName;
+              this.childLastName = childLastName;
+              this.birthDate = birthDate;
+              this.sex = sex;
+              this.relation = relation;
+              this.school = school;
+              
+	      
+    }
+
+    public String getCertNum() {
+        return certNum;
+    }
+
+    public void setCertNum(String certNum) {
+        this.certNum = certNum;
+    }
+
+    public String getChildLastName() {
+        return childLastName;
+    }
+
+    public void setChildLastName(String childLastName) {
+        this.childLastName = childLastName;
+    }
+
+    public String getChildFirstName() {
+        return childFirstName;
+    }
+
+    public void setChildFirstName(String childFirstName) {
+        this.childFirstName = childFirstName;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getRelation() {
+        return relation;
+    }
+
+    public void setRelation(String relation) {
+        this.relation = relation;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
 			  
-	  public String getEmployeeNum()
-	  {
-		  return employeenum; 
-	  }
+	 
 	  
-	  public void setEmployeeNum(String employeenum)
+    public String getChild()
 	  {
-		  this.employeenum = employeenum;
-	  }
+		  return "# " + certNum + "FN " + childFirstName + " LN " + childLastName 
+                          + " Sex " + sex + " Birthdate " + birthDate
+                          + " Relation " + relation + " School " + school;
+    }
 	  
-	  public String getChildLastname()
-	  {
-		  return childlastname; 
-	  }
 	  
-	  public void setChildLastname(String childlastname)
-	  {
-		  this.childlastname = childlastname;
-	  }
-	  
-	    
-	  public String getchildFirstname()
-	  {
-		  return childfirstname; 
-	  }
-	  
-	  public void setChildFirstname(String childfirstname)
-	  {
-		  this.childfirstname = childfirstname;
-	  }
-	  
-	  public String getChildType()
-	  {
-		  return childtype;
-	  }
-	  
-	  public void setChildType(String childtype)
-	  {
-		  this.childtype = childtype;
-	  }
-	  
-	  public String getChild()
-	  {
-		  return "# " + employeenum + "FN " + childfirstname + " LN " + childlastname + " Type " + childtype;
-	  }
-	  
-	  // getter, equals, hashcode
-	        
-	  @Override
-	  public String toString() {
-	    return String.format("%s %s %s years", childfirstname, childlastname, childtype);
-	  }
 
 }
